0dad5a0061dff1b796de1ba660246ae8e7e767ab
danielWongHin <38197275+danielWongHin@users.noreply.github.com>
2023-09-29 00:20:32 -0400

    #28 enabled static website hosting, uploaded index and error file via terraform (#29)

--- variables.tf
+++ variables.tf
@@ -4,3 +4,15 @@ variable "user_uuid" {
 variable "bucket_name" {
   type = string
 }
+
+variable "index_html_filepath" {
+  description = "The file path for index.html"
+  type        = string
+
+}
+variable "error_html_filepath" {
+  description = "The file path for error.html"
+  type        = string
+
+}
+
