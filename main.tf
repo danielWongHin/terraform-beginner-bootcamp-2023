8d785f7a6f66761a9c083c341e866265162f0c49
danielWongHin <38197275+danielWongHin@users.noreply.github.com>
2023-10-09 01:08:40 -0400

    51 multi home terraform cloud (#53)
    * added the varialbe .example file (#52)
    
    * #51 fixed issues with provider, setup two different towns

--- main.tf
+++ main.tf
@@ -13,12 +13,12 @@ terraform {
   #    name = "terra-house-1"
   #  }
   #}
-  #cloud {
-  #  organization = "ExamPro"
-  #  workspaces {
-  #    name = "terra-house-1"
-  #  }
-  #}
+  cloud {
+   organization = "daniel-terraform"
+   workspaces {
+     name = "terra-house-1"
+   }
+  }
 
 }
 
@@ -28,22 +28,38 @@ provider "terratowns" {
   token=var.terratowns_access_Token
 }
 
-module "terrahouse_aws" {
- assets_path = var.assets_path
- source = "./modules/terrahouse_aws"
+module "home_arcane_hosting" {
+ source = "./modules/terrahome_aws"
  user_uuid = var.teacherseat_user_uuid
- index_html_filepath = var.index_html_filepath
- error_html_filepath = var.error_html_filepath
- content_version = var.content_version
+ public_path = var.arcane.public_path
+ content_version = var.arcane.content_version
 }
 
 
-resource "terratowns_home" "home" {
-  name = "Success of Manchester  United !!"
+resource "terratowns_home" "home_arcane" {
+  name = "Arcane (TV series)"
   description = <<DESCRIPTION
-Manchester United Football Club, commonly referred to as Man United, or simply United, is a professional football club based in Old Trafford, Greater Manchester, England. The club competes in the Premier League, the top division in the English football league system
+Arcane (titled onscreen as Arcane: League of Legends) is an animated steampunk action-adventure television series created by Christian Linke and Alex Yee. It was produced by the French animation studio Fortiche under the supervision of Riot Games, and distributed by Netflix. 
 DESCRIPTION
-  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = module.home_arcane_hosting.domain_name
   town = "missingo"
-  content_version = 1
+  content_version = var.arcane.content_version
+}
+
+module "home_manu_hosting" {
+ source = "./modules/terrahome_aws"
+ user_uuid = var.teacherseat_user_uuid
+ public_path = var.manu.public_path
+ content_version = var.manu.content_version
+}
+
+
+resource "terratowns_home" "home_manu" {
+  name = "Manchester is Red"
+  description = <<DESCRIPTION
+Manchester United Football Club, commonly referred to as Man United, or simply United, is a professional football club based in Old Trafford, Greater Manchester, England. The club competes in the Premier League, the top division in the English football league system. 
+DESCRIPTION
+  domain_name = module.home_manu_hosting.domain_name
+  town = "missingo"
+  content_version = var.manu.content_version
 }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value       = module.terrahouse_aws.bucket_name
+  value       = module.home_arcane_hosting.bucket_name
 }
 
 output "s3_website_endpoint" {
   description = "S3 Static Website hossting endpoint"
-  value       = module.terrahouse_aws.website_endpoint
+  value       = module.home_arcane_hosting.website_endpoint
 }
 
-output "cloudfront_url" {
+output "domain_name" {
   description = "The CloudFront Distrubtion Domain"
-  value = module.terrahouse_aws.cloudfront_url
+  value = module.home_arcane_hosting.domain_name
 }
